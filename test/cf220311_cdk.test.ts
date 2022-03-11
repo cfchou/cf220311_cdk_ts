332329efd96e69e9b8f44f460b25a1354c73f678
chifengchou <chifeng@horangi.com>
2022-03-11 11:30:51 +0800

    Initial commit

---
+++ test/cf220311_cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Cf220311Cdk from '../lib/cf220311_cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cf220311_cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Cf220311Cdk.Cf220311CdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
